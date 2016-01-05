7f2908424ac97186ea694522e7b559a14ee4f23a
Max Wheeler <max@makenosound.com>
2016-01-05 13:26:11 +1100

    Export actionTypes in the main file.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import composer from './composer'
 import * as fieldActions from './actions/fields'
+import * as fieldActionTypes from './constants/action-types'
+
 export default composer
 export let actions = fieldActions
+export let actionTypes = fieldActionTypes
