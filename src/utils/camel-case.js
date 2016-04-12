3da95f8cafdc37814db54c8b43d637b5285be59e
Chris Buttery <info@chrisbuttery.com>
2016-04-12 15:47:45 +1000

    Adds try catch statement to camelCase function

--- src/utils/camel-case.js
+++ src/utils/camel-case.js
@@ -4,15 +4,20 @@
  * @return {String} UnderScore is now CamelCased
  */
 function camelCase (str, capitaliseLead = false) {
-  str = str.replace(/_([a-z])/g, (group) => {
-    if (group[1]) {
-      return group[1].toUpperCase()
+  try {
+    str = str.replace(/_([a-z])/g, (group) => {
+      if (group[1]) {
+        return group[1].toUpperCase()
+      }
+    })
+    if (capitaliseLead) {
+      return str.charAt(0).toUpperCase() + str.slice(1)
+    } else {
+      return str
     }
-  })
-  if (capitaliseLead) {
-    return str.charAt(0).toUpperCase() + str.slice(1)
-  } else {
-    return str
+  }
+  catch (err) {
+    throw new Error (err + '. `str` is ' + str)
   }
 }
 
