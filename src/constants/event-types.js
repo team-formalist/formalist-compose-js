c873ff9c9c8ad4875397af3289b5ce265f587dbe
Max Wheeler <max@makenosound.com>
2018-10-10 16:48:00 +1100

    Add form initialised/removed events

--- src/constants/event-types.js
+++ src/constants/event-types.js
@@ -2,23 +2,27 @@
  * External event constants
  */
 export const externalEvents = {
-  'FORM_CHANGE': 'change',
-  'FORM_BUSY': 'busy',
-  'FORM_IDLE': 'idle',
-  'FORM_VALID': 'valid',
-  'FORM_INVALID': 'invalid',
-  'FIELD_CHANGE': 'field:change',
-  'FIELD_REMOVED': 'field:removed',
+  FORM_CHANGE: 'change',
+  FORM_BUSY: 'busy',
+  FORM_IDLE: 'idle',
+  FORM_VALID: 'valid',
+  FORM_INVALID: 'invalid',
+  FIELD_CHANGE: 'field:change',
+  FIELD_REMOVED: 'field:removed',
+  FORM_INITIALISED: 'form:initialised',
+  FORM_REMOVED: 'field:removed',
 }
 
 /**
  * Internal event constants
  */
 export const internalEvents = {
-  'FIELD_BUSY': 'field:busy',
-  'FIELD_IDLE': 'field:idle',
-  'FIELD_VALID': 'field:valid',
-  'FIELD_INVALID': 'field:invalid',
-  'FIELD_CHANGE': 'field:change',
-  'FIELD_REMOVED': 'field:removed',
+  FIELD_BUSY: 'field:busy',
+  FIELD_IDLE: 'field:idle',
+  FIELD_VALID: 'field:valid',
+  FIELD_INVALID: 'field:invalid',
+  FIELD_CHANGE: 'field:change',
+  FIELD_REMOVED: 'field:removed',
+  FORM_INITIALISED: 'form:initialised',
+  FORM_REMOVED: 'field:removed',
 }
